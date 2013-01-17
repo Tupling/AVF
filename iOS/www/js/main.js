5d71b7422b03ac61b3dc8e971baeadb968e52b15
Dale Tupling <tupling32@hotmail.com>
2013-01-17 22:39:50 +0430

    iOS Portion of Data API's complete
    Two fully functioning Data API's for the iOS version of my application.
    Styling complete.

--- iOS/www/js/main.js
+++ iOS/www/js/main.js
@@ -21,7 +21,6 @@ $.ajax({
 			$.each(data.headlines, function() {
                     $('#featHeadline').append(
                     $('<div>')
-                    	.attr("data-filter", true)
                         .append($("<h3>" + this.headline + "</h3>"))
                         //Tried to implement images, but not all article contain images so my code crashes once it reaches one of those articles
                         //.append($("<img src="+this.images[0].url +">")
@@ -46,10 +45,38 @@ $.ajax({
 });
 }
 
-/*function getTweet () {
+function getTweet () {
     $.ajax({
+        type: "GET",
+        dataType: "json",
+        data: " ",
+        url: "http://search.twitter.com/search.json?q=mobile+development&rpp=15include_entities=true&callback=?",
+       success: function(data) {
+            console.log(data);
+                $('#tweetSection').append(
+                    $('<div>')
+                    .attr("class","tweetHeadDiv")
+                    .append($("<p>"+"Mobile Development Tweets"+"</p>")))
 
-    })
-}*/
+            $.each(data.results, function(){
+                //Shortens date to just DD/MMM/YEAR
+                $('#tweetSection').append(
+                    $('<div>').attr("class","tweetsDiv")
+                        .append($("<img src="+this.profile_image_url+">"))
+                        .append($("<h3>"+this.from_user_name+"</h3>"))
+                        .append($("<p>" + "@" + this.from_user + "</p>")
+                            .attr("class","twitHandle"))
+                        .append($("<p>" + this.created_at.substr(0,16) + "</p>")
+                            .attr("class","date"))
+                        .append($("<p>" + this.text + "</p>")
+                            .attr("class","content"))
+                        
+                    )
+            })
+        },
+            error: function() {
+            }
+    });
+}
 
 
