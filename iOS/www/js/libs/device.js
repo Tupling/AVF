c386b642e29728c5e3b358ecf250b5894a48efaa
Dale Tupling <tupling32@hotmail.com>
2013-01-25 01:39:48 +0430

    updated device.js
    i attempted to use window.device to load device information in which
    did not work so i resorted back to device.element

--- iOS/www/js/libs/device.js
+++ iOS/www/js/libs/device.js
@@ -8,9 +8,9 @@
     function onDeviceReady() {
         var element = document.getElementById('contentOutput');
 
-        element.innerHTML = 'Device Name: '     + window.device.name     + '<br />' + 
-                            'Device Cordova: '  + window.device.cordova  + '<br />' + 
-                            'Device Platform: ' + window.device.platform + '<br />' + 
-                            'Device UUID: '     + window.device.uuid     + '<br />' + 
-                            'Device Version: '  + window.device.version  + '<br />';
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                            'Device Cordova: '  + device.cordova  + '<br />' + 
+                            'Device Platform: ' + device.platform + '<br />' + 
+                            'Device UUID: '     + device.uuid     + '<br />' + 
+                            'Device Version: '  + device.version  + '<br />';
     }
